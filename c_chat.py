98bc5fcb97826a85165670fd23523fa7225c5041
Monsterbat <n32242725@gmail.com>
2024-05-15 00:09:06 +0800

    v1.0.1 muti-keyword excel

--- c_chat.py
+++ c_chat.py
@@ -5,21 +5,27 @@ Latest:2024/05/14
 # 
 from basic_function import getData as getData
 from datetime import datetime
+import openpyxl
+from openpyxl import load_workbook
 
 def c_chat_z(keyword, date, search_type):
-    pageurl="https://www.ptt.cc/bbs/C_Chat/index.html"
+    pageurl = "https://www.ptt.cc/bbs/C_Chat/index.html"
+    keyword_ori = keyword
     keyword = keyword.lower()
-    keyword_length = len(keyword)
+    keyword = keyword.split()
+    keyword_len = []
+    for keyword_ls in keyword:
+        keyword_len = keyword_len + [len(keyword_ls)]
 
-    # Creat file and clean previous data
-    with open("PTT C_Chat list.txt", "w",encoding="utf-8",newline='') as data_result_01:
-        data_result_01.write("")
+    keyword_data = {"keyword_data":[]}
+    for keyword_ls,keyword_len_ls in zip(keyword,keyword_len):
+        keyword_data["keyword_data"].append({"keyword":keyword_ls,"keyword_len":keyword_len_ls})
 
     # Choose data need analysis
     # 輸入日期
-    date_year = int(date.split("/")[0])
-    date_month = int(date.split("/")[1])
-    date_day = int(date.split("/")[2])
+    date_year = int(date.split("-")[0])
+    date_month = int(date.split("-")[1])
+    date_day = int(date.split("-")[2])
     # 當下日期
     now = datetime.now()
     now_year = int(now.year)
@@ -27,16 +33,19 @@ def c_chat_z(keyword, date, search_type):
     now_day = int(now.day)
 
     switch = True
+    # Creat file and clean previous data
+    output_json_data = {
+        "result":[
+            {"title":"標題","date":"日期"}
+        ]
+    }
 
     while switch == True:
         # 
         data_result = getData(pageurl)
         # 取出日期驗證
         page_date = data_result["result"]["require_data"][0]["date"]
-        now = datetime.now()
-        now_year = int(now.year)
-
-        page_year = int(now_year)
+        page_year = int(now_year) # 尚未進行跨年處理！
         page_month = int(page_date.split("/")[0])
         page_day = int(page_date.split("/")[1])
 
@@ -52,34 +61,69 @@ def c_chat_z(keyword, date, search_type):
 
         if switch == True:            
             require_data_result = data_result["result"]["require_data"]
-            import_data(require_data_result,search_type,keyword_length,keyword)
+            output_data = analysis_data(require_data_result,search_type,keyword_data)
+            for output_data_ls in output_data:
+                output_json_data["result"].append(output_data_ls)
 
         pageurl="https://www.ptt.cc" + data_result["result"]["url"]#換頁
     if switch == False:
+        # 寫入xlsx
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = keyword_ori
+        xlsx_data = output_json_data["result"]
+        print(xlsx_data)
+        for index, entry in enumerate(xlsx_data, start=1):
+            ws[f'A{index}'] = entry["title"]
+            ws[f'B{index}'] = entry["date"]
+        # 建立總表
+        wb = load_workbook("data.xlsx")
+        summary_sheet = wb.create_sheet(title="Summary")
+        summary_data_json = {
+            "result":[
+                {"keyword":"keyword","count":"討論數"},
+                {"keyword":keyword_ori,"count":len(xlsx_data)}
+            ]
+        }
+        summary_data = summary_data_json["result"]
+        for index, entry in enumerate(summary_data, start=1):
+            summary_sheet[f'A{index}'] = entry["keyword"]
+            summary_sheet[f'B{index}'] = entry["count"]
+        wb.save("data.xlsx")
         print("Finish!")
+    
 
-def import_data(require_data_result,search_type,keyword_length,keyword):    
-    # Importing data
-    # 變小寫
+def analysis_data(require_data_result,search_type,keyword_data):    
+    
+    # 取得資料變小寫
+    output_data = []
     require_data_result_lower = require_data_result
     for title_result_ls in require_data_result_lower:
         title_result_ls["title"] = title_result_ls['title'].lower()
+
     # 找標題
     if search_type == "title only":
         for require_data_result_lower_ls in require_data_result_lower:
             # print("!!!!!!!!",type(movie_title_filter))
             title_result_lower = require_data_result_lower_ls["title"]
-            if title_result_lower[1:keyword_length+1] == keyword:
-                with open("PTT C_Chat list.txt", "a",encoding="utf-8",newline='') as data_result_01:
-                    data_result_01.write(str(title_result_lower))
-                    data_result_01.write("\n")
-    
+
+            for keyword_data_ls in keyword_data["keyword_data"]:
+                keyword = keyword_data_ls["keyword"]
+                keyword_len = keyword_data_ls["keyword_len"]
+                if title_result_lower[1:keyword_len+1] == keyword:
+                    output_data = output_data + [require_data_result_lower_ls]
+
     # 模糊搜尋         
     if search_type == "include all":  
         for require_data_result_lower_ls in require_data_result_lower:
             title_result_lower = require_data_result_lower_ls["title"]
-            if keyword in title_result_lower:
-                # print("*****",movie_title_filter_1)
-                with open("PTT C_Chat list.txt", "a",encoding="utf-8",newline='') as data_result_01:
-                    data_result_01.write(str(title_result_lower))
-                    data_result_01.write("\n")
+
+            for keyword_data_ls in keyword_data["keyword_data"]:
+                keyword = keyword_data_ls["keyword"]
+                keyword_len = keyword_data_ls["keyword_len"]
+
+                if keyword in title_result_lower:
+                    output_data = output_data + [require_data_result_lower_ls]
+
+    print("***",output_data)
+    return output_data
--- main_ui.py
+++ main_ui.py
@@ -1,7 +1,29 @@
 import tkinter as tk
-from tkinter import filedialog
-
+from tkinter import ttk
+from tkcalendar import Calendar  # 日期選擇器，這個要另外安裝套件
+import time  # 產生今天日期用的
 from c_chat import c_chat_z as c_chat_z
+
+def today_yyyymmdd():  # 日期參數後面可能會常常用到，先做成函式。
+    year = time.localtime().tm_year
+    month = time.localtime().tm_mon
+    day = time.localtime().tm_mday
+    return year, month, day
+
+def day_choose(event):  # 如果要做成按鈕，記得把event刪掉
+    global date_inp
+    def insert_day():
+        date_inp.delete(0, 'end')  # 插入日期資料前，先刪掉本來的資料
+        selected_date = cal.selection_get()
+        formatted_date = selected_date.strftime('%Y-%m-%d')
+        date_inp.insert(0, formatted_date)  # 插入日期選擇器資料
+        top.destroy()  # 最後把彈出式視窗關掉
+    top = tk.Toplevel(window)  # 彈出式視窗
+    # 日期選擇器，日期帶入預設今天日期
+    cal = Calendar(top, font="10", selectmode='day',
+                   cursor="hand1", year=today_yyyymmdd()[0], month=today_yyyymmdd()[1], day=today_yyyymmdd()[2])
+    cal.pack(fill="both", expand=True)
+    ttk.Button(top, text="ok", command=insert_day).pack()
 
 # -*- Window -*-
 window = tk.Tk()
@@ -14,11 +36,14 @@ label1.grid(row=0, column=0)  # Position label in row 0, column 0
 entry1 = tk.Entry(window)
 entry1.grid(row=0, column=1)  # Position entry next to the label
 
-# Label and Entry for Value 2
+# Label and Entry for Date Selection
 label2 = tk.Label(window, text="截止日")
 label2.grid(row=1, column=0)  # Position label in row 1, column 0
-entry2 = tk.Entry(window)
-entry2.grid(row=1, column=1)  # Position entry next to the label
+
+date_inp = tk.Entry(window)
+date_inp.grid(row=1, column=1)  # Position entry next to the label
+date_inp.insert(0, f"{today_yyyymmdd()[0]}-{today_yyyymmdd()[1]:02d}-{today_yyyymmdd()[2]:02d}")  # 預設今天日期
+date_inp.bind('<1>', day_choose)  # 點選文字欄彈出日期選擇器
 
 # Checkbox for Value 3
 check_var = tk.IntVar()  # Variable to store the state of the checkbox
@@ -27,9 +52,10 @@ checkbox.grid(row=2, column=0, columnspan=2)  # Position the checkbox
 
 # Function to handle button click
 def handle_button_click():
+    print()
     # Get the values from the entry widgets
     value1 = entry1.get().lower()
-    value2 = entry2.get().lower()
+    value2 = date_inp.get().lower()
     # Determine the value of value3 based on the checkbox
     if check_var.get() == 1:
         value3 = "include all" 
